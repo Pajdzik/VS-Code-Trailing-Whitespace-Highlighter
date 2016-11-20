6d33fecd48efcf5e6ba82868ced2825eb4b41cc8
Kamil Pajdzik <pajdziupaj@gmail.comn>
2016-11-19 17:02:52 -0800

    Refactor extension into a class

--- trailingwhitespacehighlighter/src/extension.ts
+++ trailingwhitespacehighlighter/src/extension.ts
@@ -1,18 +1,17 @@
-'use strict';
+"use strict";
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
+import * as vscode from "vscode";
+import whitespacehighlighter = require('./whitespacehighlighter/extension');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    var extensionName = "Trailing Whitespace Highlighter"
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log(`Extension "${extensionName}" is now active!`);
+    // // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // // This line of code will only be executed once when your extension is activated
+    // console.log(`Extension "${extensionName}" is now active!`);
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
@@ -27,63 +26,7 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 
     var activeEditor = vscode.window.activeTextEditor;
-
-    if (activeEditor) {
-        triggerUpdateDecorations();
-    }
-
-    vscode.window.onDidChangeActiveTextEditor(editor => {
-        activeEditor = editor;
-        if (editor) {
-            triggerUpdateDecorations();
-        }
-    }, null, context.subscriptions);
-
-    vscode.workspace.onDidChangeTextDocument(event => {
-        if (activeEditor && event.document === activeEditor.document) {
-            triggerUpdateDecorations();
-        }
-    }, null, context.subscriptions);
-
-    var timeout = null;
-    function triggerUpdateDecorations() {
-        if (timeout) {
-            clearTimeout(timeout);
-        }
-        timeout = setTimeout(updateDecorations, 500);
-    }
-
-    function updateDecorations() {
-        if (!activeEditor) {
-            return;
-        }
-
-        var regEx = /\s+[\r\n]/g;
-
-        var text = activeEditor.document.getText();
-
-        var whiteSpaceDecoration: vscode.DecorationOptions[] = [];
-        var match;
-
-        while (match = regEx.exec(text)) {
-
-            var startPos = activeEditor.document.positionAt(match.index);
-
-            var endPos = activeEditor.document.positionAt(match.index + match[0].length);
-
-            var decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Number **' + match[0] + '**' };
-            whiteSpaceDecoration.push(decoration);
-        }
-
-        // create a decorator type that we use to decorate large numbers
-        var largeNumberDecorationType = vscode.window.createTextEditorDecorationType({
-            cursor: 'crosshair',
-            backgroundColor: 'rgba(255,0,0,0.3)'
-        });
-
-        //activeEditor.setDecorations(smallNumberDecorationType, smallNumbers);
-        activeEditor.setDecorations(largeNumberDecorationType, whiteSpaceDecoration);
-    }
+    var whitespaceHighlighterExtension = new whitespacehighlighter.WhitespaceHighlighter.Extension(activeEditor, context);
 }
 
 // this method is called when your extension is deactivated
---
+++ trailingwhitespacehighlighter/src/whitespacehighlighter/extension.ts
@@ -0,0 +1,68 @@
+import * as vscode from 'vscode';
+
+export namespace WhitespaceHighlighter {
+    export class Extension {
+        static extensionName = "Trailing Whitespace Highlighter"
+        static trailingWhitespaceRegex = /\s+(\n|$)/g  ///\s+[\r\n]/g;
+        static decorationType = vscode.window.createTextEditorDecorationType({
+            backgroundColor: 'rgba(255, 66, 178, 0.3)'
+        });
+
+        private activeEditor: vscode.TextEditor;
+        private context: vscode.ExtensionContext;
+        private timeout: NodeJS.Timer;
+
+        constructor(activeEditor: vscode.TextEditor, context: vscode.ExtensionContext) {
+            this.activeEditor = activeEditor;
+            this.context = context;
+
+            if (this.activeEditor) {
+                this.triggerUpdateDecorations();
+            }
+        }
+
+        public updateDecorations() {
+            if (!this.activeEditor) {
+                return;
+            }
+
+            var text = this.activeEditor.document.getText();
+
+            var whiteSpaceDecoration: vscode.DecorationOptions[] = [];
+            var match;
+
+            while (match = Extension.trailingWhitespaceRegex.exec(text)) {
+                var startPos = this.activeEditor.document.positionAt(match.index);
+                var endPos = this.activeEditor.document.positionAt(match.index + match[0].length);
+
+                var decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Number **' + match[0] + '**' };
+                whiteSpaceDecoration.push(decoration);
+            }
+
+            this.activeEditor.setDecorations(Extension.decorationType, whiteSpaceDecoration);
+        }
+
+        private subscribe() {
+            vscode.window.onDidChangeActiveTextEditor(editor => {
+                this.activeEditor = editor;
+                if (editor) {
+                    this.triggerUpdateDecorations();
+                }
+            }, null, this.context.subscriptions);
+
+            vscode.workspace.onDidChangeTextDocument(event => {
+                if (this.activeEditor && event.document === this.activeEditor.document) {
+                    this.triggerUpdateDecorations();
+                }
+            }, null, this.context.subscriptions);
+        }
+
+        private triggerUpdateDecorations() {
+            if (this.timeout) {
+                clearTimeout(this.timeout);
+            }
+
+            this.timeout = setTimeout(() => this.updateDecorations(), 500);
+        }
+    }
+}
